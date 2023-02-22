5d088ad2db8cd9b5a74fde6f3fef15a31ac279b8
samvi <samvi@host.docker.internal>
2023-02-22 20:50:49 +0530

    AA

--- Samvil__225/src/sam.java
+++ Samvil__225/src/sam.java
@@ -2,16 +2,22 @@
 public class sam {
 	int n,m,res;
 	
-	void add()
+	 void add()
 	{
 	 res=m+n;	
 	}
+	 void display()
+	 {
+		 System.out.println(res);
+	 }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello milk");
 		sam s1= new sam();
 		s1.add();
+		System.out.println();
+	
 
 	}
 
