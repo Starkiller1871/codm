83b6dc9b0646264528377dae1d01157b1e6c64bd
samvi <samvi@Samvil>
2023-05-03 22:47:48 +0530

    sdcfsdc

--- sam_225/src/sam_225/sam.java
+++ sam_225/src/sam_225/sam.java
@@ -1,15 +1,10 @@
+package sam_225;
 
 public class sam {
-	int n,m,res;
-	
-	void add()
-	{
-		
-	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		System.out.println("Hello univberse");
 
 	}
 
