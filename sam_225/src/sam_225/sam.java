2c89d28bfbc90ac76b156620777c34d86321ae68
samvi <samvi@Samvil>
2023-05-03 22:51:11 +0530

    sdsd

--- sam_225/src/sam_225/sam.java
+++ sam_225/src/sam_225/sam.java
@@ -4,7 +4,7 @@ public class sam {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello univberse");
+		System.out.println("Hello mama");
 
 	}
 
